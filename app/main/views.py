31765d9756785083754a9406a4a05fe2fa4e4acd
smithaldon1 <smithaldon@gmail.com>
2022-11-18 23:48:54 -0500

    base template routes and style updates

--- app/main/views.py
+++ app/main/views.py
@@ -15,3 +15,12 @@ def show_index():
     if request.method == 'GET':
         title = 'GUFC Goes National!'
         return render_template('main/index.html', title=title)
+
+@main_bp.route('/donate')
+def show_donate():
+    return render_template('partners/donation.html', title='Make a Donation')
+
+@main_bp.route('/site-map')
+def show_site_map():
+    title = 'Site Map'
+    return render_template('main/site-map.html', title=title)
