2eee4a603ba1e3f035acd75de3358b8f577847f4
smithaldon1 <smithaldon@gmail.com>
2022-11-26 18:12:07 -0500

    add content

--- app/about/views.py
+++ app/about/views.py
@@ -11,7 +11,7 @@ def show_index():
 
 @about_bp.route('/league')
 def show_league():
-    title = 'About League'
+    title = 'League'
     # return redirect(url_for('about.show_uc_page'))
     return render_template('about/league.html', title=title)
 
@@ -23,21 +23,32 @@ def show_vmc():
 
 @about_bp.route('/team')
 def show_team():
-    title = 'About Team'
+    title = 'Team'
     # return redirect(url_for('about.show_uc_page'))
     return render_template('about/team.html', title=title)
 
 @about_bp.route('/club')
 def show_club():
-    title = 'About Club'
+    title = 'Club'
+    
+    fernando = {'img_src': url_for('static', filename='img/fernando.png'), 'name': 'Fernando Zuniga', 'position': 'VP of Operations', 'description': "Mr. Zuniga enters his sixth season with the Chowan women's soccer team and fourth as head coach. During the 2020-21 season, Zuniga led the Hawks to one of their best seasons in program history sporting an 8-2-1 overall record and a 5-1-1 league record for second place in the league tables."}
+    
+    aaron = {'img_src': url_for('static', filename='img/aaron.png'), 'name': 'Fernando Zuniga', 'position': 'Director of Football', 'description': "Coach Okwei was born and raised in Osu, located in Accra, Ghana where he graduated from the Middlesbrough Soccer Academy before going on to play for the Desert Warriors who competed in the 2nd tier of Ghanaian football (soccer). Most recently, Okwei played for Greensboro International FC who compete in the UPSL"}
+    
+    luke = {'img_src': url_for('static', filename='img/luke.png'), 'name': 'Luke Staats', 'position': 'VP of Marketing', 'description': "Mr. Staats played four seasons of soccer at Lees-McRae (2008-12). He guided the team to three conference regular season and tournament titles, four straight NCAA Appearances, two trips to the NCAA Division II Sweet 16 and a national championship finalist appearance."}
+    
+    michael = {'img_src': url_for('static', filename='img/michael.png'), 'name': 'Michael McCarren', 'position': 'President', 'description': "Mr. McCarren is a business leader with 24 years of cross-functional experience in areas of business development, engineering, supply management, program management, marketing, and manufacturing. He holds 4 patents, 2 John Deere Presidents awards, 3 John Deere innovation awards, 1 Agritechnica innovation award, and 1 SPI Structure Design Award."}
+    
+    owners = [ fernando, aaron, luke, michael ]
+    
     # return redirect(url_for('about.show_uc_page'))
-    return render_template('about/club.html', title=title)
+    return render_template('about/club.html', title=title, owners=owners)
 
 @about_bp.route('/players')
 def show_players():
-    title = 'About Players'
+    title = 'Players'
     # return redirect(url_for('about.show_uc_page'))
-    return render_template('about/players.html', title=title)
+    # return render_template('about/players.html', title=title)
 
 @about_bp.route('/uc', methods=['GET', 'POST'])
 def show_uc_page():
--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,31 @@
 from . import main_bp
-from flask import render_template, request
+from flask import render_template, request, url_for
 from app import db
 
 
 @main_bp.route('/')
 def show_index():
-    
+    card1 = {
+        'img_src': url_for('static', filename='img/season-standings-2022.png'),
+        'img_title': '2022 Season Standings',
+        'title': 'card.title', 
+        'text': 'card.text'
+    }
+    card2 = {
+        'img_src': url_for('static', filename='img/season-standings-2022.png'),
+        'img_title': '2022 Season Standings',
+        'title': 'card.title', 
+        'text': 'card.text'
+    }
+    card3 = {
+        'img_src': url_for('static', filename='img/season-standings-2022.png'),
+        'img_title': '2022 Season Standings',
+        'title': 'card.title', 
+        'text': 'card.text'
+    }
+    spotlight_cards = [card1, card2, card3]
     title = 'GUFC Goes National!'
-    return render_template('main/index.html', title=title)
+    return render_template('main/index.html', title=title, cards=spotlight_cards)
 
 @main_bp.route('/donate')
 def show_donate():
