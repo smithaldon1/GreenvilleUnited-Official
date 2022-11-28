82442f28280cbe9db5da6ebf5cfb3dfc556b01b5
smithaldon1 <smithaldon@gmail.com>
2022-11-27 22:50:51 -0500

    update pages

--- app/main/views.py
+++ app/main/views.py
@@ -5,27 +5,38 @@ from app import db
 
 @main_bp.route('/')
 def show_index():
+    # Spotlight Initialization
     card1 = {
         'img_src': url_for('static', filename='img/season-standings-2022.png'),
         'img_title': '2022 Season Standings',
-        'title': 'card.title', 
-        'text': 'card.text'
+        'title': 'GUFC tops Coastal Standings!', 
+        'text': 'Greenville United finishes the 2022 season at the top of the Coastal Standings. The team finished with 7 wins, 1 tie, and 2 losses, and a cumulative 21 points.'
     }
     card2 = {
-        'img_src': url_for('static', filename='img/season-standings-2022.png'),
-        'img_title': '2022 Season Standings',
-        'title': 'card.title', 
-        'text': 'card.text'
+        'img_src': url_for('static', filename='img/conference-final.png'),
+        'img_title': 'Conference Final Image',
+        'title': 'Conference Final Nov 19!', 
+        'text': 'Check out our final game on November 19th!'
     }
     card3 = {
-        'img_src': url_for('static', filename='img/season-standings-2022.png'),
-        'img_title': '2022 Season Standings',
-        'title': 'card.title', 
-        'text': 'card.text'
+        'img_src': url_for('static', filename='img/gufc-vs-goldsboro.jpg'),
+        'img_title': 'GUFC vs Goldsboro',
+        'title': 'GUFC wins 3 - 0 against Goldsboro', 
+        'text': 'Greenville United wins the season finale with a final score of 3 to nill. Fantastic work Bucks!'
     }
     spotlight_cards = [card1, card2, card3]
+    
+    # Sponsors Initialization
+    carolina_vision = {
+        'img_src': url_for('static', filename='img/carolina-vision-logo.png'),
+        'name': 'Carolina Vision Care Logo',
+    }
+    
+    partners = []
+    sponsors = [carolina_vision]
+    
     title = 'GUFC Goes National!'
-    return render_template('main/index.html', title=title, cards=spotlight_cards)
+    return render_template('main/index.html', title=title, cards=spotlight_cards, partners=partners, sponsors=sponsors)
 
 @main_bp.route('/donate')
 def show_donate():
@@ -33,8 +44,7 @@ def show_donate():
 
 @main_bp.route('/site-map')
 def show_site_map():
-    title = 'Site Map'
-    return render_template('main/site-map.html', title=title)
+    return render_template('main/site-map.html', title='Site Map')
 
 @main_bp.route('/privacy-policy')
 def show_pp():
--- app/models.py
+++ app/models.py
@@ -1,28 +1,37 @@
 from . import db
+from sqlalchemy import Enum
 
 class User(db.Model):
     """Data model for newsletter users"""
-    
     __tablename__ = 'gufc-newsletter-users'
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-    email = db.Column(
-        db.String(255),
-        index=False,
-        unique=True,
-        nullable=False
-    )
-    created = db.Column(
-        db.DateTime,
-        index=False,
-        unique=False,
-        nullable=False
-    )
+    id = db.Column(db.Integer, primary_key=True )
+    email = db.Column(db.String(255), index=False,unique=True, nullable=False)
+    created = db.Column(db.DateTime, index=False, unique=False, nullable=False)
     
     def __repr__(self):
         return '<User {}>'.format(self.email)
+
+class Player(db.Model):
+    __tablename__ = 'gufc-team-roster'
+    id = db.Column(db.Integer, primary_key=True)
+    img_src = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    position = db.Column(db.String(3), unique=False, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    
+    def __repr__(self):
+        return f"Player(id='{self.id}', img_src='{self.img_src}', name='{self.name}', pos='{self.position}, desc='{self.description}')"
+    
+class Staff(db.Model):
+    __tablename__ = 'gufc-staff-roster'
+    id = db.Column(db.Integer, primary_key=True)
+    img_src = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    position = db.Column(db.String(3), unique=False, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    
+    def __repr__(self):
+        return f"Staff(id='{self.id}', img_src='{self.img_src}', name='{self.name}', pos='{self.position}, desc='{self.description}')"
     
 class Card(db.Model):
     __tablename__ = 'gufc-cards'
